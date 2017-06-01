728f29a13d2871386212ce0e8a08b11a19f52ef5
Maxim Tychkov <tichkov_maxim@mail.ru>
2017-06-01 17:41:58 +0600

    Initial commit

--- src/main/java/com/epam/tm/news/controller/RestNewsController.java
+++ src/main/java/com/epam/tm/news/controller/RestNewsController.java
@@ -1,12 +1,15 @@
 package com.epam.tm.news.controller;
 
+import com.epam.tm.news.entity.News;
 import com.epam.tm.news.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 @RestController
@@ -16,8 +19,16 @@ public class RestNewsController {
     @Autowired
     NewsService newsService;
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String helloWorld(){
-        return newsService.getAllNewsByDate().get(0).getBrief();
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String helloWorld(@PathVariable("id") long id){
+
+        List<News> allNewsByDate = newsService.getAllNewsByDate();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (News news : allNewsByDate) {
+            stringBuilder.append(news);
+            stringBuilder.append("</br>");
+        }
+        return stringBuilder.toString();
     }
 }
