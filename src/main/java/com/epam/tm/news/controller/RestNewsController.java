bd598f11128f497c61c1cbce608bc34c75b310e7
Maxim Tychkov <tichkov_maxim@mail.ru>
2017-06-07 18:03:31 +0600

    add spring security

--- src/main/java/com/epam/tm/news/controller/RestNewsController.java
+++ src/main/java/com/epam/tm/news/controller/RestNewsController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class RestNewsController implements NewsController<ResponseEntity> {
 
     @Override
     @RequestMapping(value = "", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity getAllNewsByDate() throws ControllerException {
         try {
             List<News> allNewsByDate = newsService.getAllNewsByDate();
